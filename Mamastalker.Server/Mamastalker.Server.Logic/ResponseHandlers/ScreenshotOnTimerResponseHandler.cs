f515525de23f5b7a7b78bbe291a0ff95212a9119
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 08:30:20 +0200

    implement initial response handler

---
+++ Mamastalker.Server/Mamastalker.Server.Logic/ResponseHandlers/ScreenshotOnTimerResponseHandler.cs
@@ -0,0 +1,33 @@
+﻿using Mamastalker.Server.Logic.ResponseHandlers.Abstract;
+using System;
+using System.Threading.Tasks;
+
+namespace Mamastalker.Server.Logic.ResponseHandlers
+{
+    public class ScreenshotOnTimerResponseHandler<TData> : IResponseHandler<TData>
+    {
+        private Action<byte[]> _listeningCallback;
+
+        private async Task UpdateLoop()
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+
+                SendScreenshot();
+            }
+        }
+
+        private void SendScreenshot()
+        {
+            // var byteData;
+
+            // _listeningCallback?.Invoke(byteData);
+        }
+
+        public void HandleData(TData data, Action<byte[]> reply)
+        {
+            _listeningCallback += (byteData) => reply?.Invoke(byteData);
+        }
+    }
+}
