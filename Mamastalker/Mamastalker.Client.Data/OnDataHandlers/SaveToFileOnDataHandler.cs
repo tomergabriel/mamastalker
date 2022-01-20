e0e4526e5f2d9e05c22a6a94ef3dd5ea36184ded
TomerGabriel <tomergabriel@gmail.com>
2022-01-20 11:23:22 +0200

    create save to file data handler

---
+++ Mamastalker/Mamastalker.Client.Data/OnDataHandlers/SaveToFileOnDataHandler.cs
@@ -0,0 +1,11 @@
+﻿using Mamastalker.Client.Logic.OnDataHandlers.Abstract;
+
+namespace Mamastalker.Client.Data.OnDataHandlers
+{
+    public class SaveToFileOnDataHandler<TData, TOut> : IOnDataHandler<TData>
+    {
+        public void OnDataEventHandler(TData data)
+        {
+        }
+    }
+}
